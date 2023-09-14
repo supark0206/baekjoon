f38db03c608815aa3da1435566580a8445f2ea87
supark0206 <supark0206@gmail.com>
2023-09-14 15:16:21 +0900

    Initial commit

---
+++ src/bronze1/평균은넘겠지_4344/Main.java
@@ -0,0 +1,16 @@
+package bronze1.평균은넘겠지_4344;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution() {
+        return 0;
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
