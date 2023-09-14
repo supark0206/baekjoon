e52e86239f5ea65271a0f505e1d83ac0d8d382ad
supark0206 <supark0206@gmail.com>
2023-09-14 16:30:55 +0900

    commit

--- src/bronze1/평균은넘겠지_4344/Main.java
+++ src/bronze1/평균은넘겠지_4344/Main.java
@@ -1,16 +1,52 @@
 package bronze1.평균은넘겠지_4344;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public int solution() {
-        return 0;
+    public void solution(ArrayList<int[]> array) {
+
+        String answer = "";
+
+        for (int[] n : array) {
+            int sum = 0;
+            int avg = 0;
+            float cnt = 0;
+
+            for (int score : n) {
+                sum += score;
+                avg = sum/n.length;
+            }
+
+            for (int score : n) {
+                if(score > avg) {
+                    cnt++;
+                }
+            }
+            float per = (cnt*100)/n.length;
+            answer = String.format("%.3f", per) + "%";
+            System.out.println(answer);
+        }
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        ArrayList<int[]> array = new ArrayList<>();
+        
+        for(int i=0; i<n; i++){
+            int m = sc.nextInt();
+            int[] score = new int[m];
+            
+            for(int j=0; j<m; j++){
+                score[j] = sc.nextInt();
+            }
+            array.add(score);
+        }
+
+        main.solution(array);
 
     }
 }
