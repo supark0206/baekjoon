44435f066f6e5d45d22e55f0bf7b318d615ff164
supark0206 <supark0206@gmail.com>
2023-09-18 22:30:31 +0900

    commit

--- src/bronze1/여름나기_18130/Main.java
+++ src/bronze1/여름나기_18130/Main.java
@@ -6,34 +6,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int fan = sc.nextInt();
-        int home = sc.nextInt();
+        int N = sc.nextInt();
+        int Q = sc.nextInt();
         int price = Integer.MAX_VALUE;
-        int fanNum = 0;
+        int NNum = 0;
 
-        for(int i=0; i<fan; i++){
+        for(int i=0; i<N; i++){
             int P = sc.nextInt(); // 선풍기가격
             int K = sc.nextInt(); // 추가비용 내는 시간
             int C  = sc.nextInt(); // 추가비용 초기값
 
             int addPrice = 0;
 
-            int num = home/K;
-            if(home%K == 0) num -= 1;
+            int num = Q/K;
+            if(Q%K == 0) num -= 1;
 
 
-            for(int j=1; j<=num; j++){
-                addPrice = addPrice + (C * j) ;
-            }
-
-            addPrice += P ;
+            addPrice = C * (num*(num+1)/2) + P;
 
             if(addPrice<price){
-                fanNum = i;
+                NNum = i;
             }
             price = Math.min(price,addPrice);
         }
 
-        System.out.println(fanNum+1 + " " + price);
+        System.out.println(NNum+1 + " " + price);
     }
 }
---
+++ src/bronze1/초콜릿자르기_2163/Main.java
@@ -0,0 +1,15 @@
+package bronze1.초콜릿자르기_2163;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println(x*y-1);
+
+    }
+}
